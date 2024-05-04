e4e86b1c3e77024decff6d571123f729ceac600a
Micah Sherry <MicahJSherry@gmail.com>
2024-05-03 22:55:46 -0400

    remove test functions for cylinders

--- Code/Main.py
+++ Code/Main.py
@@ -1,12 +1,6 @@
 from Quadrature import integrate
 from spline import *
 
-
-
-def f(x):
-    return 2
-def g(x):
-    return 1
 def function_Circleizer(f):
     """ Given a funtion return a new Function that will calculate the Area of a circle with radius f(x) 
     """
